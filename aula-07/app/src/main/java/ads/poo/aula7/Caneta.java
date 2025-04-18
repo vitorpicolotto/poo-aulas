cb59f7b35266d1d7c4f073eef4ff3123dad41182
vitorpicolotto <picolottovitor@gmail.com>
2025-04-18 11:25:00 -0300

    métodos e execução - aula 07

--- aula-07/app/src/main/java/ads/poo/aula7/Caneta.java
+++ aula-07/app/src/main/java/ads/poo/aula7/Caneta.java
@@ -6,6 +6,7 @@ public class Caneta {
     private String cor;
     private double nivelTinta;
     private boolean tampa;
+    private int d;
     
     //metodos
     public void definirCor(String c){
@@ -18,6 +19,17 @@ public class Caneta {
 
     public void definirNivelTinta(double n){
         nivelTinta = n;
+        if(nivelTinta > 1.0){ 
+            nivelTinta = 1.0;
+        } else if(nivelTinta <0.0){
+            nivelTinta = 0.0;
+        } else {
+            nivelTinta = n;
+        }
+    }
+
+    public double getNivelTinta(){
+        return nivelTinta;
     }
 
     public void abrirFechar(boolean t){
@@ -27,5 +39,38 @@ public class Caneta {
     public boolean isAberta(){
         return tampa;
     }
+
+
+    public String desenhar(int d){
+        String desenhoTraco = "-";
+
+        //1. se a caneta estiver aberta e com tinta, desenha
+        //2. calcular quantas linhas conisgue desenhar com a quantidade de tinta que tem
+                //se d = 1, desenha uma linha, se d=2 desenha duas linhas (retorna uma string com a quantidade de linhas)...
+        //3. a cada linha desenhada, diminui o nivel de tinta em 0.1
+        //4. return "-".repeat(d)
+        //se o nivel de tinta for menor que 0.1, não desenha e retorna uma string dizendo que não tem tinta
+
+        if(isAberta()){
+            if(nivelTinta > 0.1){
+                if(d > 0 && d <= 10){
+                    if(nivelTinta >= d * 0.1){
+                        nivelTinta -= d * 0.1;
+                        return desenhoTraco.repeat(d);
+                    } else {
+                        return "Não tem tinta suficiente para desenhar " + d + " linhas.";
+                    }
+                } else {
+                    return "Desenho inválido!";
+                }
+            } else {
+                return "Não tem tinta suficiente para desenhar.";
+            }
+
+        }
+        return desenhar(d);
+    }
+
+
 
 }
